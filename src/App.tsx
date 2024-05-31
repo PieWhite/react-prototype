49bbe0ab7243e6d7f641bcd8673f7588fe9f0013
dieptevrees <dieptevrees@gmail.com>
2024-05-31 12:18:06 +0000

    Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import CounterButton from './components/CounterButton'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,9 +20,7 @@ function App() {
       <h1>Vite + React</h1>
       <h2>Plan je Trip Prototype</h2>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <CounterButton count={count} setCount={setCount} />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
---
+++ src/components/CounterButton.tsx
@@ -0,0 +1,8 @@
+const CounterButton = ({ count, setCount }) => {
+  return (
+    <button onClick={() => setCount((count) => count + 1)}>
+      count is {count}
+    </button>
+  );
+};
+export default CounterButton;
